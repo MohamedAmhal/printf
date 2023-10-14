92146988a5bbae26dc7a058b8cdc3ee66aeb70eb
MohamedAMHAL <mohammedamhal349@gmail.com>
2023-10-14 16:27:14 -0700

    updating the file _printf.c

--- _printf.c
+++ _printf.c
@@ -1,11 +1,19 @@
 #include "main.h"
 
 /**
- * _printf - function that produces output according to a format
- * @format: character string
- * @...: other variables
- * Return: number of characters printed
+ * prt_char - print a char '%c'
+ * @word: declaration
+ * Return: 1
  */
+
+int prt_char(va_list word)
+{
+	char loc;
+
+	loc = va_arg(word, int);
+	_putchar(loc);
+	return (1);
+}
 
 int _printf(const char *format, ...)
 {
