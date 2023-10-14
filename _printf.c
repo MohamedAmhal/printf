0e7c7ca7bea98b558accc9c2d325db31d42025ed
azich <aaziza.chbihi@gmail.com>
2023-10-14 16:41:27 -0700

    function that puts strings

--- _printf.c
+++ _printf.c
@@ -15,6 +15,37 @@ int prt_char(va_list word)
 	return (1);
 }
 
+/**
+ * prt_str - print a string '%s'
+ * @word: declaration
+ * Return: the length of a string
+ */
+
+int ptr_dtr(va_list worf)
+{
+	char *str;
+	int len;
+	int i;
+
+	str = va_arg(val, char *);
+	
+	if (!str)
+	{
+		str = "(null)";
+		len = _strlen(str);
+	}
+	else
+	{
+		len = _strlen(str);
+	}
+
+	for (i = 0; i < len; i++)
+		_putchar(str[i]);
+
+	return (len);
+}
+
+
 int _printf(const char *format, ...)
 {
 
