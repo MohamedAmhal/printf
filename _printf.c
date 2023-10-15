0b77aede93be90b562bce730e31c7a5956558c52
azich <aaziza.chbihi@gmail.com>
2023-10-14 17:14:30 -0700

    betty documentation

--- _printf.c
+++ _printf.c
@@ -60,7 +60,7 @@ int prt_pers(void)
  * _printf - produces output according to a format
  * @format: character string
  * @...: other arguments
- * Return: number of characters printed 
+ * Return: number of characters printed
  */
 
 int _printf(const char *format, ...)
