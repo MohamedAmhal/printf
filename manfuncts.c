7cdcc4511c54d92447dba87ab8ec898446f619ae
MohamedAMHAL <mohammedamhal349@gmail.com>
2023-10-16 15:28:24 -0700

    updating the function prt_str

--- manfuncts.c
+++ manfuncts.c
@@ -30,13 +30,21 @@ int prt_str(va_list word)
 	str = va_arg(word, char *);
 
 	if (str == NULL)
+	{
 		str = "(null)";
-	len = _strlen(str);
+		len = _strlen(str);
+		for (i = 0; i < len; i++)
+			_putchar(str[i]);
+		return (len);
+	}
+	else
+	{
+		len = _strlen(str);
+		for (i = 0; i < len; i++)
+			_putchar(str[i]);
 
-	for (i = 0; i < len; i++)
-		_putchar(str[i]);
-
-	return (len);
+		return (len);
+	}
 }
 
 /**
