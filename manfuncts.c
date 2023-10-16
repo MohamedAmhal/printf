3431803efe309a10e0813547fec5ba03a50efcd0
azich <aaziza.chbihi@gmail.com>
2023-10-16 15:01:35 -0700

    printf

--- manfuncts.c
+++ manfuncts.c
@@ -103,40 +103,41 @@ int prt_int(va_list nmb)
 
 int prt_dec(va_list nmb)
 {
-        int a = va_arg(nmb, int);
-        int number, digit;
-        int end = a % 10;
-        int x = 1;
-        int i = 1;
+	int a = va_arg(nmb, int);
+	int number, digit;
+	int end = a % 10;
+	int x = 1;
+	int i = 1;
 
-        a = a / 10;
-        number = a;
+	a = a / 10;
+	number = a;
 
-        if (end < 0)
-        {
-        _putchar('-');
-                number = -number;
-                a = -a;
-        end = -end;
-        i++;
-        }
-        if (number > 0)
-        {
-        while (number / 10 != 0)
-        {
-                x = x * 10;
-                number = number / 10;
-        }
-        number = a;
-                while (x > 0)
-        {
-                digit = number / x;
-                _putchar(digit + '0');
-                number = number - (digit * x);
-                x = x / 10;
-                i++;
-                }
-        }
-        _putchar(end + '0');
-        return (i);
+	if (end < 0)
+	{
+		_putchar('-');
+		number = -number;
+		a = -a;
+		end = -end;
+		i++;
+	}
+
+	if (number > 0)
+	{
+		while (number / 10 != 0)
+		{
+			x = x * 10;
+			number = number / 10;
+		}
+		number = a;
+		while (x > 0)
+		{
+			digit = number / x;
+			_putchar(digit + '0');
+			number = number - (digit * x);
+			x = x / 10;
+			i++;
+		}
+	}
+	_putchar(end + '0');
+	return (i);
 }
