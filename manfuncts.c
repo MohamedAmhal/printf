8bc9f48c48dc4ee6eab576c2310d2f284658e82a
MohamedAMHAL <mohammedamhal349@gmail.com>
2023-10-16 13:51:40 -0700

    updating the file mandatory

--- manfuncts.c
+++ manfuncts.c
@@ -94,3 +94,49 @@ int prt_int(va_list nmb)
 	_putchar(end + '0');
 	return (i);
 }
+
+/**
+ * prt_dec - this is the function print a decimal
+ * @nmb: the variadic variable
+ * Return: the lenght
+ */
+
+int prt_int(va_list nmb)
+{
+        int a = va_arg(nmb, int);
+        int number, digit;
+        int end = a % 10;
+        int x = 1;
+        int i = 1;
+
+        a = a / 10;
+        number = a;
+
+        if (end < 0)
+        {
+        _putchar('-');
+                number = -number;
+                a = -a;
+        end = -end;
+        i++;
+        }
+        if (number > 0)
+        {
+        while (number / 10 != 0)
+        {
+                x = x * 10;
+                number = number / 10;
+        }
+        number = a;
+                while (x > 0)
+        {
+                digit = number / x;
+                _putchar(digit + '0');
+                number = number - (digit * x);
+                x = x / 10;
+                i++;
+                }
+        }
+        _putchar(end + '0');
+        return (i);
+}
